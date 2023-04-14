04564d89381a09dc977041857d4652b7489c37f2
Juan <jriveraa102@gmail.com>
2023-04-13 23:37:52 -0400

    added create reminder tab

--- M4/beta/app.js
+++ M4/beta/app.js
@@ -2,11 +2,16 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
 
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost/reminders', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.error('Could not connect to MongoDB'));
+// Set up view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Connect to MongoDB Atlas
+mongoose.connect('mongodb+srv://juan:nob@cluster0.9wsduos.mongodb.net/reminders?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB Atlas'))
+  .catch((err) => console.error('Could not connect to MongoDB Atlas'));
 
 // Define Reminder schema and model
 const reminderSchema = new mongoose.Schema({
@@ -15,15 +20,18 @@ const reminderSchema = new mongoose.Schema({
   date: Date,
 });
 
-const Reminder = mongoose.model('Reminder', reminderSchema);
+const Reminder = mongoose.model('Reminder', reminderSchema, 'reminders');
 
 // Set up middleware
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('public'));
 
 // Define routes
-app.get('/', (req, res) => {
-  res.send('Welcome to my reminder app!');
+app.get('/', async (req, res) => {
+  const reminders = await Reminder.find({});
+  res.render('index', { reminders });
 });
+
 
 // Get all reminders
 app.get('/reminders', async (req, res) => {
@@ -31,8 +39,13 @@ app.get('/reminders', async (req, res) => {
   res.render('reminders', { reminders });
 });
 
-// Add new reminder
-app.post('/reminders', async (req, res) => {
+// Add new reminder - display form
+app.get('/addReminder', async (req, res) => {
+  res.render('addReminder');
+});
+
+// Add new reminder - handle form submission
+app.post('/addReminder', async (req, res) => {
   const { title, description, date } = req.body;
   const reminder = new Reminder({ title, description, date });
   await reminder.save();
